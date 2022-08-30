9956a4d7c0c65145b791fbb326fdaa9ad73e795e
tatjana444 <107064710+tatjana444@users.noreply.github.com>
2022-08-30 12:43:55 +0300

    commit in lambda

--- src/Person.java
+++ src/Person.java
@@ -3,7 +3,6 @@ public class Person {
     private String surname;
     private int age;
 
-
     public Person(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
